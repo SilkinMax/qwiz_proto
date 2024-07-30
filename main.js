cc23895250a9571ac5c9e21f97794175e6003b0b
Silkin Max <m.silkin@websoft.ru>
2024-07-30 16:55:30 +0300

    промежуток

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 (function ()
 {
-	function askRandomQuestion(array,countCorrectAnswer)
+	function askRandomQuestion(array)
 	{
 		const question = array[parseInt(Math.random() * 4)];
-		question.askQuestion(countCorrectAnswer);
+		question.displayQuestion();
 	}
 	function Question(question, answers, correctNum)
 	{
@@ -12,15 +12,22 @@
 		this.correctNum = correctNum;
 	}
 	Question.prototype.displayQuestion = function(){
-		
+			console.log(this.question);
+			this.answers.forEach(function(elem,index) 
+			{
+				console.log(`${index}. ${elem}`);
+			});
 	}
 
-	let q1 = new Question("Какая должность у Максима?", ["Программист" ,"Водитель" ,"Учитель","Пекарь"], 1);
-	let q2 = new Question("Сколько Максиму лет?", ["26" ,"27","28" ,"29"], 4);
-	let q3 = new Question("На кого учился Максим?", ["Программист" ,"Инженер" ,"Доктор","Няня"], 2);
-	let q4 = new Question("Какой самый лучший язык программирования?", ["C#" ,"PHP", "JS"], 3);
+	let q1 = new Question("Какая должность у Максима?", ["Программист" ,"Водитель" ,"Учитель","Пекарь"], 0);
+	let q2 = new Question("Сколько Максиму лет?", ["26" ,"27","28" ,"29"], 3);
+	let q3 = new Question("На кого учился Максим?", ["Программист" ,"Инженер" ,"Доктор","Няня"], 1);
+	let q4 = new Question("Какой самый лучший язык программирования?", ["C#" ,"PHP", "JS"], 2);
 
 	const allQuestions = [q1,q2,q3,q4]
 	askRandomQuestion(allQuestions);
+
+	const answerNum = prompt("Выберите правильный вариант ответа");
+
 })();
 
